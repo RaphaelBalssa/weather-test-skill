84f8d68b81e5127b76abd287f6a719549759cd21
balssar <Raphael.balssa@etu.univ-grenoble-alpes.fr>
2019-06-05 16:28:07 +0200

    ne trouve rien dans la BDD mais le probleme n'a pas l'air d'etre ici

--- __init__.py
+++ __init__.py
@@ -1,15 +1,70 @@
-from mycroft import MycroftSkill, intent_file_handler
+import time
+from datetime import datetime
+import datetime as dt
+from copy import copy
+import json
 
+import mycroft.audio
+from adapt.intent import IntentBuilder
+from mycroft.skills.core import MycroftSkill, intent_handler, intent_file_handler
+from pyowm import OWM
+from mycroft.dialog import DialogLoader
+from mycroft.api import Api
+from mycroft.messagebus.message import Message
+from mycroft.util.format import nice_time
+from mycroft.util.log import LOG
+from mycroft.util.parse import extract_datetime
+from mycroft.util.format import nice_number
+from requests import HTTPError
+import sqlite3
+
+try:
+    from mycroft.util.time import to_utc, to_local
+except Exception:
+    import pytz
+
+class LocationNotFoundError(ValueError):
+    pass
+
+APIErrors = (LocationNotFoundError, HTTPError)
 
 class WeatherTest(MycroftSkill):
     def __init__(self):
-        MycroftSkill.__init__(self)
+        super().__init__("WeatherTest")
 
     @intent_file_handler('test.weather.intent')
-    def handle_test_weather(self, message):
-        self.speak_dialog('test.weather')
+    def handle_test_weather_f(self, message):
+        self.handle_test_weather(message)   
 
+    # Handle: idk for the moment
+    @intent_handler(IntentBuilder("").require("Query").optionally(
+        "Date").optionally("Location").build())
+    def handle_test_weather(self, message):
+
+        try:
+
+            res={}
+            connection = sqlite3.connect("anotherCompany.db")
+
+            c = connection.cursor()
+            
+
+            c.execute("SELECT degree FROM DAILY_FORECAST WHERE strftime('%Y-%m-%d','date') == date('now')") 
+            res["degree"] = c.fetchone()
+            self.speak_dialog('test.weather', res)
+            connection.commit()
+            connection.close()
+        except APIErrors as e:
+            self.__api_error(e)
+        except Exception as e:
+            LOG.exception("Error: {0}".format(e))
+
+    def __api_error(self, e):
+        if isinstance(e, LocationNotFoundError):
+            self.speak_dialog('location.not.found')
+        elif e.response.status_code == 401:
+            from mycroft import Message
+            self.bus.emit(Message("mycroft.not.paired"))
 
 def create_skill():
     return WeatherTest()
-
